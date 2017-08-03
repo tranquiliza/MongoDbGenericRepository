51b70fbde30f9a040033c42bc5b80cc39734dc91
Tranquiliza <daniel.olsen.server@gmail.com>
2017-08-03 21:40:19 +0200

    Finished some of the DbContextWrapper

--- GenericMongoDb.Data/MongoDbContext.cs
+++ GenericMongoDb.Data/MongoDbContext.cs
@@ -1,26 +1,44 @@
 ﻿using MongoDB.Driver;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GenericMongoDb.Data
 {
+    /// <summary>
+    /// Wrapper class for the MongoClient
+    /// </summary>
     public class MongoDbContext
     {
-        private static string DatabaseName = "";
-        private static string ConnectionString = "";
+        /// <summary>
+        /// Name of the Database we want data from
+        /// </summary>
+        private static string DatabaseName = "MyDatabase";
+        
+        /// <summary>
+        /// The connection string (IP-Adress + Port)
+        /// </summary>
+        private static string ConnectionString = "mongodb://localhost:27017";
+        
+        /// <summary>
+        /// Us as a client
+        /// </summary>
         private static MongoClient Client;
-
+        
         public MongoDbContext()
         {
             if (Client == null)
                 Client = new MongoClient(ConnectionString);
         }
 
-        public void GetDatabaseContext()
+        /// <summary>
+        /// Returns the Database with the given name in the configuration.
+        /// </summary>
+        /// <returns>IMongoDatabase instance</returns>
+        public IMongoDatabase GetDatabase()
         {
+            if (Client != null)
+                return Client.GetDatabase(DatabaseName);
+
+            throw new NullReferenceException("Client not instantiated?");
         }
     }
 }
