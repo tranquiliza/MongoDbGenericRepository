16b1a0e46a81b0fadc2ae31f7f96d7b5822ce519
Tranquiliza <daniel.olsen.server@gmail.com>
2017-08-03 21:31:34 +0200

    Add project files.

---
+++ GenericMongoDb.Core/Repository/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using GenericMongoDb.Core.Domain;
+using System.Collections.Generic;
+
+namespace GenericMongoDb.Core.Repository
+{
+    public interface IBaseRepository<T> where T : Entity
+    {
+        IEnumerable<T> GetAll();
+
+        T GetByID(int id);
+
+        void Add(T entity);
+
+        void Delete(T entity);
+    }
+}
---
+++ GenericMongoDb.Data/MongoDbContext.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMongoDb.Data
+{
+    public class MongoDbContext
+    {
+        private static string DatabaseName = "";
+        private static string ConnectionString = "";
+        private static MongoClient Client;
+
+        public MongoDbContext()
+        {
+            if (Client == null)
+                Client = new MongoClient(ConnectionString);
+        }
+
+        public void GetDatabaseContext()
+        {
+        }
+    }
+}
